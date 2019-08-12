a6d1d7b2b198cb191c7d655a878979d69c6ebc6b
Thomas Carrier <amacool0117@gmail.com>
2019-08-12 14:55:55 +0300

    fix error handling bug

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ class App extends Component {
     // googleDocConnect({
     //   clientId: '443792400890-10euqlru0cmiat2f3vblrei3olp32g4j.apps.googleusercontent.com',
     //   apiKey: 'AIzaSyC01VeeK9JfUExHR1bPi37RdpoSptDg-3U',
-    //   documentId: '1OqkILmweYQR2AYd2D0_BVcFOz-Y7gpiNJeaovTcbZR0'
+    //   documentId: '1-uOj6quBuvs7wWidJfHuBz4PgN6Hq2dmjl27H_BjWy0'
     // }).then(res => {
-    //    getSectionBlocks(res);
+    //    console.log(res.result);
+    //    data = getSectionBlocks(res.result);
+    //    this.forceUpdate();
     // });
     // console.log(getSectionBlocks(dummyData));
     
